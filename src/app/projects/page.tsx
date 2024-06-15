3263a59b39816e48d83f7c13be6453ae4167144a
mikuull <mmikulski643@gmail.com>
2024-06-15 23:22:01 +0200

    feat: add projects page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <>
+      <h1 className="text-3xl text-white/60 p-72">
+        There aren't any projects yet...
+      </h1>
+    </>
+  );
+};
+
+export default Projects;
