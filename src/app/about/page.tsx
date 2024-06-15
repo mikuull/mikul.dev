f55ffd3767222ff985eeb65d963e968672c46d70
mikuull <mmikulski643@gmail.com>
2024-06-15 19:19:21 +0200

    feat: add tooltip

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -18,19 +18,11 @@ import {
 const about = {
   title: "About me",
   description:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quibusdam, sunt explicabo inventore.",
+    "Hey!, I'm motivated 17 years old Frontend Developer from Poland. I have started my coding journey when I was 13, writing first Minecraft plugin in Java. I'm focused now on Web Development, especially on NodeJS. I love to recreate UI/UX designs and i want to learn NestJS in the future to become Full-Stack Developer.",
   info: [
     {
       fieldName: "Name",
       fieldValue: "Mateusz Mikulski",
-    },
-    {
-      fieldName: "Phone",
-      fieldValue: "(+48) 000000000",
-    },
-    {
-      fieldName: "Experience",
-      fieldValue: "3+ Years",
     },
     {
       fieldName: "Email",
@@ -38,7 +30,7 @@ const about = {
     },
     {
       fieldName: "Languages",
-      fieldValue: "English, Polish (Native)",
+      fieldValue: "English (B2), Polish (Native)",
     },
   ],
 };
@@ -46,7 +38,7 @@ const about = {
 const skills = {
   title: "My skills",
   description:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quibusdam, sunt explicabo inventore.",
+    "As a passionate and dedicated Frontend Developer, I bring a diverse set of skills and experiences to create dynamic and engaging web applications.",
   skillList: [
     {
       icon: <FaHtml5 />,
@@ -99,8 +91,92 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
+
 const About = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 0.4, duration: 0.4, ease: "easeIn" },
+        }}
+        className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+      >
+        <div className="container mx-auto">
+          <Tabs
+            defaultValue="about-me"
+            className="flex flex-col xl:flex-row gap-[60px]"
+          >
+            <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+              <TabsTrigger value="about-me">About Me</TabsTrigger>
+              <TabsTrigger value="my-skills">My Skills</TabsTrigger>
+            </TabsList>
+            <div className="min-h-[70vh] w-full">
+              <TabsContent
+                value="about-me"
+                className="w-full text-center xl:text-left"
+              >
+                <div className="flex flex-col gap-[30px]">
+                  <h3 className="text-3xl font-bold">{about.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                    {about.description}
+                  </p>
+                  <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+                    {about.info.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="flex items-ccenter justify-center xl:justify-start gap-4"
+                        >
+                          <span className="text-white/60">
+                            {item.fieldName}
+                          </span>
+                          <span className="text-xl">{item.fieldValue}</span>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </TabsContent>
+              <TabsContent value="my-skills" className="w-full h-full">
+                <div className="flex flex-col gap-[30px]">
+                  <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                    <h3 className="text-3xl font-bold">{skills.title}</h3>
+                    <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                      {skills.description}
+                    </p>
+                  </div>
+                  <ul className="grid grid-cols-2 sm: grid-cols-3 md:grid-cols-4 xl:gap-[30px] gap-4">
+                    {skills.skillList.map((skill, index) => {
+                      return (
+                        <li key={index}>
+                          <TooltipProvider delayDuration={50}>
+                            <Tooltip>
+                              <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                                <div className="text-6xl group-hover:text-accent transition-all duration-300">
+                                  {skill.icon}
+                                </div>
+                              </TooltipTrigger>
+                              <TooltipContent>
+                                <p className="capitalize">{skill.name}</p>
+                              </TooltipContent>
+                            </Tooltip>
+                          </TooltipProvider>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </TabsContent>
+            </div>
+          </Tabs>
+        </div>
+      </motion.div>
+    </>
+  );
 };
 
 export default About;
