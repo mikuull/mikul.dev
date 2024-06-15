6cffb3a2183b29be534364d1e7063efd257fa47d
mikuull <mmikulski643@gmail.com>
2024-06-15 18:20:19 +0200

    refactor: delete two useless pages

--- src/app/resume/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Resume = () => {
-  return <div>page</div>;
-};
-
-export default Resume;
--- src/app/services/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Services = () => {
-  return <div>page</div>;
-};
-
-export default Services;
