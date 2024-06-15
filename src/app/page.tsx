3637c457e3d66b27937c29933e19232a70eb7fec
mikuull <mmikulski643@gmail.com>
2024-06-15 17:08:48 +0200

    feat: home page hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,41 @@
 import Header from "@/components/Header";
+import Socials from "@/components/Socials";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <section className="h-full">
+        <div className="container mx-auto h-full">
+          <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+            <div className="text-center xl:text-left">
+              <span className="text-xl">Frontend Developer</span>
+              <h1 className="h1 mb-6">
+                Hello I'm <br /> <span className="text-accent">Mikul</span>
+              </h1>
+              <p className="max-w-[500px] mb-9 text-white/80">
+                I excel at crafting elegant digital experiences and I am
+                proficient in various programming languages and technologies
+              </p>
+              <div className="flex flex-col xl:flex-row items-center gap-8">
+                <Button
+                  variant={"outline"}
+                  size={"lg"}
+                  className="uppercase flex items-center gap-2 border-accent rounded-xl text-accent hover:text-white"
+                >
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+                <div className="mb-8 xl:mb-0">
+                  <Socials />
+                </div>
+              </div>
+            </div>
+            <div>photo</div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
