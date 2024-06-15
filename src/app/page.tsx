a2aa6280e48ecfad569476e0a4ccfc86a8f79232
mikuull <mmikulski643@gmail.com>
2024-06-15 18:19:16 +0200

    feat: add photo component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import Photo from "@/components/Photo";
 import Socials from "@/components/Socials";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
@@ -6,11 +7,11 @@ import { FiDownload } from "react-icons/fi";
 export default function Home() {
   return (
     <>
-      <section className="h-full">
+      <section className="h-full mt-24">
         <div className="container mx-auto h-full">
           <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-            <div className="text-center xl:text-left">
-              <span className="text-xl">Frontend Developer</span>
+            <div className="text-center xl:text-left order-2 xl:order-none">
+              <span className="text-xl text-white/60">Frontend Developer</span>
               <h1 className="h1 mb-6">
                 Hello I'm <br /> <span className="text-accent">Mikul</span>
               </h1>
@@ -32,7 +33,9 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div>photo</div>
+            <div className="order-1 xl:order-none mb-8 xl:mb-0">
+              <Photo />
+            </div>
           </div>
         </div>
       </section>
