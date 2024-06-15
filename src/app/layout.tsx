35bbc3960c4eea797de855595d4773b7e470b931
mikuull <mmikulski643@gmail.com>
2024-06-15 16:25:50 +0200

    feat: add transition provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
 
 const jetbrains_mono = JetBrains_Mono({
   subsets: ["latin"],
@@ -23,7 +24,7 @@ export default function RootLayout({
     <html lang="en" className={jetbrains_mono.className}>
       <body>
         <Header />
-        {children}
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
