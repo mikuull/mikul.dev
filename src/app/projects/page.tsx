abead1d607742542df908e720b6fd9cec7ef4953
mikuull <mmikulski643@gmail.com>
2024-06-16 12:55:08 +0200

    feat: add projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const Projects = () => {
   return (
     <>
-      <h1 className="text-3xl text-white/60 p-72">
-        There aren't any projects yet...
-      </h1>
+      <div className="flex flex-col items-center">
+        <h1 className="xl:text-3xl text-white/60 xl:p-72 pt-40 px-6 pb-3 text-2xl">
+          There aren't any projects yet...
+        </h1>
+        <Button
+          variant={"outline"}
+          size={"lg"}
+          className="rounded-2xl border-accent"
+        >
+          <Link href={"/"}>Back</Link>
+        </Button>
+      </div>
     </>
   );
 };
