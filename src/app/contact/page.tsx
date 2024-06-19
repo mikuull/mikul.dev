a97bfb4d63040d3afe8b1f95aeff4d8b7822b4a0
mikuull <mmikulski643@gmail.com>
2024-06-19 19:55:20 +0200

    refactor: contact page refactor

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,8 @@
 "use client";
 import React, { useState } from "react";
-
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.NEXT_EMAIL_API_KEY);
-
 import {
   Select,
   SelectContent,
@@ -17,14 +12,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-
-import {
-  FaPhoneAlt,
-  FaEnvelope,
-  FaMapMarkedAlt,
-  FaMapMarkerAlt,
-} from "react-icons/fa";
-import { title } from "process";
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const info = [
   {
@@ -39,24 +28,21 @@ const info = [
   },
   {
     icon: <FaMapMarkerAlt />,
-    title: "Adress",
+    title: "Address",
     description: "Warszawa, Polska",
   },
 ];
 
-import { motion } from "framer-motion";
-
 const Contact = () => {
   const [formData, setFormData] = useState({
-    name: "",
-    surname: "",
+    firstname: "",
+    lastname: "",
     email: "",
+    service: "",
     message: "",
   });
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
+  const handleChange = (e: any) => {
     const { name, value } = e.target;
     setFormData((prevFormData) => ({
       ...prevFormData,
@@ -64,18 +50,20 @@ const Contact = () => {
     }));
   };
 
+  const handleServiceChange = (value: string) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      service: value,
+    }));
+  };
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      await resend.emails.send({
-        from: "onboarding@resend.dev",
-        to: "mmikulski643@gmail.com",
-        subject: "Email from mikul.dev",
-        html: `<p>Email od: <strong>${formData.email}</strong> </br> Imię: <strong>${formData.name}</strong> </br> Nazwisko: <strong>${formData.surname}</strong> </br> Treść: <strong>${formData.message}</strong></p>`,
-      });
-      //email sent successfully
+      alert("Form submitted successfully!");
     } catch (err) {
-      console.log(err);
+      console.error(err);
+      alert("Error submitting form");
     }
   };
 
@@ -93,8 +81,8 @@ const Contact = () => {
           <div className="flex flex-col xl:flex-row gap-[30px]">
             <div className="xl:h-[54%] order-2 xl:order-none">
               <form
-                className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
                 onSubmit={handleSubmit}
+                className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
               >
                 <h3 className="text-4xl text-accent">Let's work together</h3>
                 <p className="text-white/60">
@@ -103,47 +91,54 @@ const Contact = () => {
                 </p>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                   <Input
-                    type="firstname"
-                    id={"name"}
+                    type="text"
+                    name="firstname"
                     placeholder="First name"
+                    value={formData.firstname}
                     onChange={handleChange}
                   />
                   <Input
-                    type="lastname"
-                    id={"surname"}
+                    type="text"
+                    name="lastname"
                     placeholder="Last name"
+                    value={formData.lastname}
                     onChange={handleChange}
                   />
                   <Input
                     type="email"
-                    id={"email"}
+                    name="email"
                     placeholder="Email"
+                    value={formData.email}
                     onChange={handleChange}
                   />
                 </div>
-                <Select>
+                <Select onValueChange={handleServiceChange}>
                   <SelectTrigger className="w-full">
                     <SelectValue placeholder="Select a service" />
                   </SelectTrigger>
                   <SelectContent>
                     <SelectGroup>
                       <SelectLabel>Select a service</SelectLabel>
-                      <SelectItem value="est">Web Development</SelectItem>
-                      <SelectItem value="cst">UI/UX Design</SelectItem>
-                      <SelectItem value="mst">Logo Design</SelectItem>
+                      <SelectItem value="Web Development">
+                        Web Development
+                      </SelectItem>
+                      <SelectItem value="UI/UX Design">UI/UX Design</SelectItem>
+                      <SelectItem value="Logo Design">Logo Design</SelectItem>
                     </SelectGroup>
                   </SelectContent>
                 </Select>
                 <Textarea
                   className="h-[200px]"
-                  id={"message"}
+                  name="message"
                   placeholder="Type your message here"
+                  value={formData.message}
                   onChange={handleChange}
                 />
                 <Button
                   size={"lg"}
                   variant={"outline"}
                   className="max-w-40 rounded-2xl border-2 border-accent hover:text-primary"
+                  type="submit"
                 >
                   Send
                 </Button>
@@ -151,19 +146,17 @@ const Contact = () => {
             </div>
             <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
               <ul className="flex flex-col gap-10">
-                {info.map((item, index) => {
-                  return (
-                    <li key={index} className="flex items-center gap-6">
-                      <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-xl flex items-center justify-center">
-                        <div className="text-[28px]">{item.icon}</div>
-                      </div>
-                      <div className="flex-1">
-                        <p className="text-white/60">{item.title}</p>
-                        <h3 className="text-xl">{item.description}</h3>
-                      </div>
-                    </li>
-                  );
-                })}
+                {info.map((item, index) => (
+                  <li key={index} className="flex items-center gap-6">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-xl flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>
+                      <h3 className="text-xl">{item.description}</h3>
+                    </div>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
